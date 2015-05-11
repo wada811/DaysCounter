2a1b7f569b72636b1b047ff539c55b701e78b34d
wada811 <at.wada811@gmail.com>
2015-05-12 08:55:57 +0900

    Add Joda-Time Android and init

---
+++ app/src/main/java/at/wada811/dayscounter/App.java
@@ -0,0 +1,13 @@
+package at.wada811.dayscounter;
+
+import android.app.Application;
+import net.danlew.android.joda.JodaTimeAndroid;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JodaTimeAndroid.init(this);
+    }
+}
